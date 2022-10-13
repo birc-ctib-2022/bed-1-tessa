c1c4f58616e6afd09d0ae1807a4201905b100737
tessdiv <tessadivita@gmail.com>
2022-10-13 20:13:44 +0200

    attempt again to fix line 62 and 63 of query_bed.py

--- src/query_bed.py
+++ src/query_bed.py
@@ -59,8 +59,8 @@ def main() -> None:
        
         for index_chromosome_list in chromosome_list: 
             bed_chromosome = chromosome_list[0]
-            bed_chromosome_start = chromosome_list(line[1])
-            bed_chromosome_end = chromosome_list(line[2])
+            bed_chromosome_start = chromosome_list(chromosome_list[1])
+            bed_chromosome_end = chromosome_list(chromosome_list[2])
 
             if query_chromosome_start <= bed_chromosome_start and query_chromosome_end>= bed_chromosome_end:
                 print_line(index_chromosome_list,args.outfile)
